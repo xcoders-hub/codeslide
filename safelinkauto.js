bb7bb04ecd8ee778a7d4d680740fcd9611ee0559
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2021-04-02 21:19:19 +0530

    Create safelinkauto.js

---
+++ safelinkauto.js
@@ -0,0 +1,18 @@
+<script type='text/javascript'>
+protected_links =&#39;facebook.com,twitter.com,youtube.com,blogger.com,instagram.com,gumroad.com,mukeshtemplate.blogspot.com,1.bp.blogspot.com,2.bp.blogspot.com,3.bp.blogspot.com,4.bp.blogspot.com,5.bp.blogspot.com,t.me,gum.co,https://&#39;;
+auto_safelink();
+  </script>
+  
+<script type='text/javascript'>
+//<![CDATA[
+var
+setting=
+{
+    exceptionurl:"mukeshtemplate.blogspot.com,gum.co,gumroad.com,google.com,twitter.com,whatsapp.com,1.bp.blogspot.com,2.bp.blogspot.com,3.bp.blogspot.com,4.bp.blogspot.com,5.bp.blogspot.com,blogger.com,wa.me,vk.com,dmca.com,paypal.me,paypal.com,google.com,twitter.com,whatsapp.com,blogger.com,instagram.com,facebook.com,#,javascript:,tel:,mailto:",
+    path:"#?o="
+    
+};
+//]]></script>
+<script src='https://cdn.jsdelivr.net/gh/Indzign/theme@master/autosafelink.js'></script>
+
+<script src='https://www.blogger.com/feeds/5885710407473589797/posts/default?alt=json-in-script&amp;max-results=150&amp;callback=showurl'></script>
