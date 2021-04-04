68bbc64796a407e94f8420dab6d69c12a3436008
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2021-04-04 05:38:09 +0530

    Create tag_generator.php

---
+++ tag_generator.php
@@ -0,0 +1,42 @@
+<?php
+if($_POST['submit']){
+$key= $_POST['moviename'];
+$actor= $_POST['actorname'];
+$netflixer= $_POST['netfix'];
+
+echo '9xmovies,';
+echo  $key.' movie download,';
+echo  $key.' hindi dubbed, ';
+echo  $key.'hindi dubbed  free download,';
+echo  $key.' '.$netflixer.' free download,';
+echo  $key.' on quadplay.xyz free,';
+echo  $actor .$key.' movie,';
+echo  $actor.'movies,';
+echo 'download 1 gb movies free,';
+echo 'Download '.$key.' full movies in hd dubbed,';
+echo 'download '.$key.' movie, ';
+echo 'download '.$key.' '.$netflixer.' film for free,';
+echo 'download english movies,';
+echo 'download film '.$key.',';
+echo 'download tt11388278, ';
+echo 'drama mvoies,';
+echo 'dubbed movies on quadplay,';
+echo 'Family films free download,';
+echo 'free '.$netflixer.',';
+echo 'Free '.$netflixer.' hindi dubbed movies download,';
+echo 'free '.$netflixer.' shows download,';
+echo 'free teen movies download,';
+echo 'Khatrimaza,';
+echo 'Musical movies free download,';
+echo ''.$netflixer.' films free download,';
+echo $key;
+echo  $key.' world4free,';
+echo 'world4ufree';
+}
+?>
+<form action="" method="post">
+<input type="text" name="moviename" placeholder="moviename">
+<input type="text" name="netflixer" placeholder="true or false">
+<input type="text" name="actorname" placeholder="actorname">
+<button type="submit" name="submit">Submit</button>
+</form>
