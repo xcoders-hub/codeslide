271622e4f2d787cac7c453a78a3511c5c115fc57
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2021-05-06 14:13:19 +0530

    Create java classes.java

---
+++ java classes.java
@@ -0,0 +1,4 @@
+public boolean isOnline() {
+        NetworkInfo activeNetworkInfo = ((ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
