d204a87b145ff1c3f72278edf84412a1ec460605
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2021-05-31 02:06:38 +0530

    Update getdirectoryfilesasapi.php

--- getdirectoryfilesasapi.php
+++ getdirectoryfilesasapi.php
@@ -12,5 +12,30 @@ foreach ($files as $file){
 }else {
     echo base64_decode('R2V0dGluZyBOdWxsIFJlc3BvbnNl');
 }
+
+#####Later We try ####
 ?>
+<?php
+if($_GET['apikey'] == 'connect'){
+$path = '/videos';
+$jl=  '[';
+$jr=  ']';
+echo $jl.fileset().$jr;
+}else {
+    echo base64_decode('R2V0dGluZyBOdWxsIFJlc3BvbnNl');
+}
+function fileset(){
+$serveruri= 'http://educke.com/videos/';
+$files = glob("*.mp4");
+foreach ($files as $file){
+    if(is_file($file)){
+    //   return $file; //$data = '{"video":[{"video_url":"'.$file.'"}]}'
+    $vidurl= '{"video_url": "'.$serveruri.rawurlencode($file).'" },';
+     echo $vidurl;
+    }
+}
+}
+?>
+
+
 
