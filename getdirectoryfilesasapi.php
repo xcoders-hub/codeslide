62cd46ea2028a1d2de868850a079ba490b002740
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2021-05-31 01:24:54 +0530

    Create getdirectoryfilesasapi.php

---
+++ getdirectoryfilesasapi.php
@@ -0,0 +1,16 @@
+<?php
+$path = '/';
+$files = glob("*.mp4");
+$serveruri= ' ';
+
+if($_GET['apikey'] == 'connect'){
+foreach ($files as $file){
+    if(is_file($file)){
+        echo $data = '{"video_url":"'.$serveruri.rawurlencode($file).'"}'; //$data = '{"video":[{"video_url":"'.$file.'"}]}'
+    }
+}
+}else {
+    echo base64_decode('R2V0dGluZyBOdWxsIFJlc3BvbnNl');
+}
+?>
+
